1011153ca0c34b090262f6687e6935a0bb7b979d
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-08-07 18:05:50 +0200

    mongoose test

--- api/index.js
+++ api/index.js
@@ -1,10 +1,17 @@
 const app = require('express')();
 
+const { User } = require('./models.js');
 const connectDB = require('./mongoose.js');
 
 app.get('/api', async (req, res) => {
     await connectDB();
 
+    console.log("Adding user to DB");
+    const newUser = new User({ username: "ExtraFrens", refreshToken: "API" });
+    
+    await newUser.save();
+    console.log("Done!");
+    
     res.send('Hello World!');
 });
 
