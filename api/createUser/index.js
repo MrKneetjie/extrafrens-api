3760db70109c2c733911781f0972f9d347262d71
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-08-07 21:00:09 +0200

    createUser

--- api/createUser/index.js
+++ api/createUser/index.js
@@ -1,3 +1,4 @@
+import { Account } from "../models";
 const connectDB = require('../mongoose.js');
 
 export default async function handler(req, res) {
@@ -8,41 +9,22 @@ export default async function handler(req, res) {
             message: "Missing required fields",
         });
     } else {
-        res.status(200).json({
-            body: req.body,
-            query: req.query,
-            cookies: req.cookies,
+        await Account.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            role: req.body.role,
+            thumb: req.body.thumb,
+        }).then((account) => {
+            res.status(200).json({
+                message: "Account created successfully",
+                account: account,
+            });
+        }).catch((err) => {
+            res.status(500).json({
+                message: "Error creating account",
+                error: err,
+            });
         });
     }
 }
-
-// import { Account } from "./models";
-// const connectDB = require('./mongoose.js');
-
-// export default async function handler(req, res) {
-//     await connectDB();
-    
-//     if (!req.body.name || !req.body.email || !req.body.password || !req.body.role || !req.body.thumb) {
-//         res.status(400).json({
-//             message: "Missing required fields",
-//         });
-//     } else {
-//         await Account.create({
-//             name: req.body.name,
-//             email: req.body.email,
-//             password: req.body.password,
-//             role: req.body.role,
-//             thumb: req.body.thumb,
-//         }).then((account) => {
-//             res.status(200).json({
-//                 message: "Account created successfully",
-//                 account: account,
-//             });
-//         }).catch((err) => {
-//             res.status(500).json({
-//                 message: "Error creating account",
-//                 error: err,
-//             });
-//         });
-//     }
-// }
