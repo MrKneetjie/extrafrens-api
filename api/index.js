c44e43bea055c6001db0f71bc6492df5515d1f60
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-08-07 20:05:21 +0200

    test

--- api/index.js
+++ api/index.js
@@ -12,8 +12,11 @@ app.post('/api/createUser', async (req, res) => {
 
     // const newUser = new Account({ name: req.body.name, email: req.body.email, password: req.body.password, thumb: req.body.thumb, role: req.body.role });
     // await newUser.save();
-
-    res.send(req.body.name);
+    if (req.body.name) {
+        res.send("Name is filled");
+    } else {
+        res.send("Name is empty");
+    }
 });
 
 module.exports = app;
