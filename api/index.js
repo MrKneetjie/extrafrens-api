b8e2b6def07c5c2a265952a90e5eb2fe26b52e88
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-08-07 19:56:57 +0200

    createUser init

--- api/index.js
+++ api/index.js
@@ -3,16 +3,17 @@ const app = require('express')();
 const { Account } = require('./models.js');
 const connectDB = require('./mongoose.js');
 
-app.get('/api', async (req, res) => {
-    await connectDB();
-
-    console.log("Adding user to DB");
-    const newUser = new Account({ username: "ExtraFrens", password: "TEST" });
-    
-    await newUser.save();
-    console.log("Done!");
-    
+app.get('/api', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/createUser', async (req, res) => {
+    await connectDB();
+    
+    // const newUser = new Account({ name: req.body.name, email: req.body.email, password: req.body.password, thumb: req.body.thumb, role: req.body.role });
+    // await newUser.save();
+
+    res.send(req.body);
 });
 
 module.exports = app;
