f7b6ad9201f68accd83b4e700e956b92df0ed170
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-08-07 21:37:32 +0200

    re-added check

--- api/createUser/index.js
+++ api/createUser/index.js
@@ -4,21 +4,27 @@ const connectDB = require('../mongoose.js');
 export default async function handler(req, res) {
     await connectDB();
 
-    await Account.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password,
-        role: req.body.role,
-        thumb: req.body.thumb,
-    }).then((account) => {
-        res.status(200).json({
-            message: "Account created successfully",
-            account: account,
+    if (!req.body.name || !req.body.email || !req.body.password || !req.body.role || !req.body.thumb) {
+        res.status(400).json({
+            message: "Missing required fields",
         });
-    }).catch((err) => {
-        res.status(500).json({
-            message: "Error creating account",
-            error: err,
+    } else {
+        await Account.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+            role: req.body.role,
+            thumb: req.body.thumb,
+        }).then((account) => {
+            res.status(200).json({
+                message: "Account created successfully",
+                account: account,
+            });
+        }).catch((err) => {
+            res.status(500).json({
+                message: "Error creating account",
+                error: err,
+            });
         });
-    });
+    }
 }
