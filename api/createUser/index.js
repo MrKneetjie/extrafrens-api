e5e70b66a9a8f8cf367f9f660b40bf8f5244dd23
David Knetemann <david.knetemann@studiohyperdrive.be>
2022-08-09 21:15:52 +0200

    cors test

--- api/createUser/index.js
+++ api/createUser/index.js
@@ -1,9 +1,27 @@
 import { Account } from "../models";
 const connectDB = require('../mongoose.js');
 
-export default async function handler(req, res) {
+const allowCors = fn => async (req, res) => {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    // another common pattern
+    // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    if (req.method === 'OPTIONS') {
+      res.status(200).end()
+      return
+    }
+    return await fn(req, res)
+}
+  
+  
+async function handler(req, res) {
     await connectDB();
-
+    
     if (!req.body.name || !req.body.email || !req.body.password || !req.body.role || !req.body.thumb) {
         res.status(400).json({
             message: "Missing required fields",
@@ -28,3 +46,5 @@ export default async function handler(req, res) {
         });
     }
 }
+
+module.exports = allowCors(handler)
